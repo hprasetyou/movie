c36e4ffbf462e2a79272f3e555652b0a325f6541
Heru Prasetyo Utomo <hprasetyou@gmail.com>
2019-02-26 21:37:31 +0700

    install twitter bootstrap

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import VueRouter from 'vue-router'
 import App from './App.vue'
 import { routes } from './routes'
 
+import 'bootstrap/dist/css/bootstrap.css'
+
 Vue.use(VueRouter)
 
 const router = new VueRouter({
