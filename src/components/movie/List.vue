7dcf4c9a2c7a9e9bc2fe0aece70cbd84676f6e29
Heru Prasetyo Utomo <hprasetyou@gmail.com>
2019-02-26 22:22:40 +0700

    make template for movie list

--- src/components/movie/List.vue
+++ src/components/movie/List.vue
@@ -1,3 +1,55 @@
 <template>
-    <h1>This is list page</h1>
+  <div>
+    <h1>Movie list</h1>
+    <div class="movie-list row">
+      <div class="col-sm-3" v-for="(movie, key) in movies" :key="key">
+        <a :href="movie.slug" class="movie-list-item">
+          <div class="border rounded">
+            <div class="movie-list-item--image">
+              <img :src="movie.cover" alt="">
+            </div>
+            <div class="movie-list-item--info-wrapper">
+              <h3 class="movie-list-item--title">{{ movie.title }}</h3>
+              <p>{{ movie.year }}</p>
+            </div>
+          </div>
+        </a>
+      </div>
+    </div>
+  </div>
 </template>
+<script>
+  export default {
+    data() {
+      return {
+        movies: [{
+          slug: 'wiro-sableng',
+          cover: 'https://lorempixel.com/400/200/',
+          title: 'Wiro sableng',
+          year: 1995,
+        }]
+      }
+    }
+  }
+
+</script>
+<style lang="less">
+  .movie-list-item {
+
+    &,
+    &:hover {
+      color: inherit;
+    }
+
+    &:hover {
+      text-decoration: none;
+    }
+
+    &--image {
+      img {
+        width: 100%;
+      }
+    }
+  }
+
+</style>
