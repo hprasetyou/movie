de129642c7c1b3ab9c48914d6f103324fcb27ee5
Heru Prasetyo Utomo <hprasetyou@gmail.com>
2019-02-27 19:26:42 +0700

    movie detail layout

--- src/components/movie/Detail.vue
+++ src/components/movie/Detail.vue
@@ -1,12 +1,32 @@
 <template>
   <div>
-    <h4>this is detail page</h4>
-    {{ $route.params.slug }}
+    <div class="row">
+      <div class="col-lg-4">
+        <div class="movie-cover">
+          <img :src="movie.cover" alt="">
+          <p>Duration {{ movie.duration }}</p>
+        </div>
+      </div>
+      <div class="col-lg-8">
+          <div class="movie-title border-bottom">
+            <h4>{{ movie.title }}</h4>
+            <span class="float-right">Rating {{ movie.vote_average }}</span>
+            <span>Language: {{ movie.languages }}</span>
+          </div>
+          <div class="movie-description mb-lg-4">
+            <p>{{ movie.overview }}</p>
+          </div>
+          <div class="movie-info">
+            <span>Genres: </span><span class="font-italic">{{ movie.genres }}</span> 
+          </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
 import axios from 'axios';
 import * as conf from '../../config.js';
+import _ from 'lodash';
 export default {
   data() {
     return {
@@ -17,8 +37,16 @@ export default {
     this.movie.id = this.$route.params.slug.split('-')[0];
     axios.get(`${conf.ApiUrl}/${this.movie.id}?api_key=${conf.ClientKey}`).then((
         response) => {
-        let movies = response.data;
-        console.log(movies);
+        let movie = response.data;
+        this.$set(this.movie,'title', movie.title);
+        this.$set(this.movie,'cover',`https://image.tmdb.org/t/p/w500${movie.poster_path}`);
+        this.$set(this.movie,'overview', movie.overview);
+        this.$set(this.movie,'languages',_.map(movie.spoken_languages,'name').join(','))
+        this.$set(this.movie,'genres',_.map(movie.genres,'name').join(','))
+        this.$set(this.movie,'vote_average', movie.vote_average)
+        this.$set(this.movie,'duration', Math.floor(movie.runtime/60)+' hour '+(movie.runtime%60)+' min' )
+        
+        console.log(movie);
         
         
     }).catch(function (error) {
@@ -28,3 +56,11 @@ export default {
   },
 }
 </script>
+<style lang="less">
+     .movie-cover{
+       img{
+         max-width: 100%;
+       }
+     }
+</style>
+
