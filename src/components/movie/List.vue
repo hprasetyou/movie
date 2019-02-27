1bf37031a547eb46398840570edcce7958412bf0
Heru Prasetyo Utomo <hprasetyou@gmail.com>
2019-02-27 22:59:05 +0700

    handle query url

--- src/components/movie/List.vue
+++ src/components/movie/List.vue
@@ -3,7 +3,7 @@
     <h1>Movie list</h1>
     <div class="movie-list row mx-md-0">
       <div :class="['col-md-3','px-md-2','mb-sm-3']" v-for="(movie, key) in movies" :key="key">
-        <movieItem :title="movie.title" :slug="movie.slug" :cover="movie.cover" :date="movie.date" :rating="movie.rating"/>
+        <movieItem :title="movie.title" :slug="movie.slug" :cover="movie.cover" :date="movie.date" :rating="movie.rating" />
       </div>
     </div>
   </div>
@@ -20,29 +20,36 @@
     },
     data() {
       return {
-        movies: []
+        movies: [],
+        page:1
+      }
+    },
+    methods: {
+      getMovies(page) {
+        axios.get(`${conf.ApiUrl}/now_playing?api_key=${conf.ClientKey}&region=ID&page=${page}`).then((
+          response) => {
+          let movies = response.data.results;
+          this.movies = _.map(movies, function (movie) {
+            return {
+              title: movie.title,
+              slug: movie.id + '-' + movie.title.toLowerCase().replace(' ', '-'),
+              cover: 'https://image.tmdb.org/t/p/w500' + movie.poster_path,
+              date: movie.release_date,
+              rating: movie.vote_average
+            }
+          })
+
+        }).catch(function (error) {
+          // handle error
+          console.log(error);
+        })
       }
     },
     mounted() {
-      axios.get(`${conf.ApiUrl}/now_playing?api_key=${conf.ClientKey}&region=ID`).then((
-        response) => {
-          console.log(this);
-          
-        let movies = response.data.results;
-        this.movies = _.map(movies, function(movie){
-            return {
-                title: movie.title,
-                slug: movie.id + '-' + movie.title.toLowerCase().replace(' ','-'),
-                cover: 'https://image.tmdb.org/t/p/w500' + movie.poster_path,
-                date: movie.release_date,
-                rating: movie.vote_average
-            }
-        })
-        
-      }).catch(function (error) {
-        // handle error
-        console.log(error);
-      })
+      if(this.$route.query.page){
+        this.page = this.$route.query.page;
+      }
+      this.getMovies(this.page);
     }
   }
 
