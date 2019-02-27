9334d51cf8b0dbc0fa18622fac5cf79fada728de
Heru Prasetyo Utomo <hprasetyou@gmail.com>
2019-02-27 20:43:30 +0700

    create navbar

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
 <template>
-  <div id="app" class="container">
-    <router-view></router-view>
+  <div id="app">
+    <layout-header />
+    <div class="container">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+  import layoutHeader from './components/layout/Header.vue'
+  export default {
+    name: 'app',
+    components: {
+      'layout-header': layoutHeader
+    },
+    data() {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
     }
   }
-}
-</script>
 
+</script>
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container-fluid sticky-top px-0">
+    <nav class="navbar navbar-expand-lg navbar-secondary bg-secondary">
+      <div class="container">
+        <a class="navbar-brand text-light" href="/">TokoFlix</a>
+        <span class="navbar-nav ml-auto text-light">Balance: 100000</span>
+      </div>
+    </nav>
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
