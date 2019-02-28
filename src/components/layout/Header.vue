ca30960328cb967d8f600fd6aa5730568cf9eb87
Heru Prasetyo Utomo <hprasetyou@gmail.com>
2019-02-28 22:52:39 +0700

    add search component

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -2,17 +2,22 @@
   <div class="container-fluid sticky-top px-0">
     <nav class="navbar navbar-expand-lg navbar-secondary bg-secondary">
       <div class="container">
-          <router-link tag="a" class="navbar-brand text-light" :to="'/'">
-            TokoFlix
-          </router-link>
-        <a class="navbar-brand text-light" href="/"></a>
+        <router-link tag="a" class="navbar-brand text-light" :to="'/'">
+          TokoFlix
+        </router-link>
+        <search-form></search-form>
       </div>
     </nav>
   </div>
 </template>
 <script>
-import { balanceMixins } from '../../balanceMixins.js';
-export default {
-  mixins: [balanceMixins]
-}
+  import { balanceMixins } from '../../balanceMixins.js';
+  import searchForm from './searchForm.vue';
+  export default {
+    components:{
+      'search-form':searchForm
+    },
+    mixins: [balanceMixins]
+  }
+
 </script>
---
+++ src/components/layout/searchForm.vue
@@ -0,0 +1,26 @@
+<template>
+  <form class="form-inline">
+    <div class="input-group">
+      <input type="text" class="form-control" v-model="searchInput" placeholder="I want to watch..." 
+        aria-describedby="button-addon2">
+      <div class="input-group-append">
+        <button class="btn btn-primary" v-on:click="submitForm" type="button" id="button-addon2">Search</button>
+      </div>
+    </div>
+  </form>
+</template>
+<script>
+  export default {
+    data(){
+        return{
+            searchInput:''
+        }
+    },
+    methods: {
+      submitForm() {
+        this.$router.push({ path: '', query: {search: this.searchInput }})
+      }
+    }
+  }
+
+</script>
