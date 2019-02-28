f22446990eee713a27ddf71639b9e6be3c77758f
Heru Prasetyo Utomo <hprasetyou@gmail.com>
2019-02-28 12:49:54 +0700

    styling movie list item

--- src/components/movie/ListItem.vue
+++ src/components/movie/ListItem.vue
@@ -6,8 +6,8 @@
       </div>
       <div class="movie-list-item--info-wrapper p-md-3">
         <h3 class="movie-list-item--title">{{ title }}</h3>
-        <span class="float-right pt-md-2">{{ date }}</span>
-        <star-rating v-model="rating" :read-only="true" :increment="0.5" :star-size="15"></star-rating>  
+        <span class="float-right pt-md-2">{{ year }}</span>
+        <star-rating v-model="rating" :read-only="true" :show-rating="false" :increment="0.5" :star-size="15"></star-rating>  
         {{ getPrice(rating) }}
       </div>
     </div>
@@ -30,6 +30,11 @@
       cover: String,
       title: String,
       rating: Number,
+    },
+    computed:{
+      year(){
+        return this.date.split('-')[0]
+      }
     }
   }
 
@@ -50,6 +55,10 @@
       img {
         width: 100%;
       }
+    }
+
+    &--title{
+      font-size: inherit;
     }
   }
 
