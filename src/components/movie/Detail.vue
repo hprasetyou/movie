df7fd0f72285c6dd4e301ed7990837daf335cee6
Heru Prasetyo Utomo <hprasetyou@gmail.com>
2019-02-28 12:02:25 +0700

    add movie recommendations on detail

--- src/components/movie/Detail.vue
+++ src/components/movie/Detail.vue
@@ -25,12 +25,23 @@
           <button v-if="isAffordable() && !isInCollections(movie.id)" @click="addToCollection" class="btn btn-primary">Add to Collections</button>
       </div>
     </div>
+    <div class="section-recommendation">
+      <div class="section--title border-bottom py-lg-3 my-sm-3">
+        <h3 class="my-lg-0">Recommendations</h3>
+      </div>
+      <div class="movie-list row mx-md-0">
+        <div :class="['col-md-2','px-md-2','mb-sm-3']" v-for="(movie, key) in recommendations" :key="key">
+          <movie-item :title="movie.title" :slug="movie.slug" :cover="movie.cover" :date="movie.date" :rating="movie.rating" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
 import axios from 'axios';
 import * as conf from '../../config.js';
 import _ from 'lodash';
+import movieListItem from './ListItem.vue';
 import { priceMixins } from '../../priceMixins.js';
 import { collectionsMixins } from '../../collectionsMixins.js';
 import { balanceMixins } from '../../balanceMixins.js';
@@ -38,16 +49,22 @@ import StarRating from 'vue-star-rating';
 
 export default {
   components:{
-    'star-rating':StarRating
+    'star-rating':StarRating,
+    'movie-item': movieListItem
   },
   data() {
     return {
-      movie:{}
+      movie:{},
+      recommendations:[],
+      similar:[]
     }
   },
   mixins:[priceMixins,collectionsMixins,balanceMixins],
   mounted() {
-    this.getMovieInfo();
+    let movieId = this.$route.params.slug.split('-')[0];
+    this.movie.id = movieId;
+    this.getMovieInfo(movieId);
+    this.getRecommendations(movieId);
   },
   methods: {
     addToCollection(){
@@ -69,9 +86,8 @@ export default {
         this.$set(this.movie,'vote_average', movie.vote_average)
         this.$set(this.movie,'duration', Math.floor(movie.runtime/60)+' hour '+(movie.runtime%60)+' min' )
     },
-    getMovieInfo(){
-        this.movie.id = this.$route.params.slug.split('-')[0];
-        axios.get(`${conf.ApiUrl}/${this.movie.id}?api_key=${conf.ClientKey}`).then((
+    getMovieInfo(movieId){
+        axios.get(`${conf.ApiUrl}/${movieId}?api_key=${conf.ClientKey}`).then((
             response) => {
             let movie = response.data;
             this.setMovieInfo(movie);      
@@ -79,6 +95,28 @@ export default {
             // handle error
             console.error(error);
         })
+    },
+    getRecommendations(movieId){
+        axios.get(`${conf.ApiUrl}/${movieId}/recommendations?api_key=${conf.ClientKey}`).then((
+            response) => {
+            let movies = response.data.results;    
+            this.recommendations = this.transformMoviesDataCollections(movies);
+            
+        }).catch(function (error) {
+            // handle error
+            console.error(error);
+        })
+    },
+    transformMoviesDataCollections(movies){
+      return _.map(movies, function (movie) {
+        return {
+          title: movie.title,
+          slug: movie.id + '-' + movie.title.toLowerCase().replace(' ', '-'),
+          cover: 'https://image.tmdb.org/t/p/w500' + movie.poster_path,
+          date: movie.release_date,
+          rating: movie.vote_average
+        }
+      })
     }
   }
 }
