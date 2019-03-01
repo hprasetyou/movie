882f340638613b306d715ab0b4f620d7ffd0f1f4
Heru Prasetyo Utomo <hprasetyou@gmail.com>
2019-03-01 13:42:01 +0700

    indicator movie in collection on detail page

--- src/main.js
+++ src/main.js
@@ -3,12 +3,12 @@ import VueRouter from 'vue-router';
 import App from './App.vue';
 import { routes } from './routes';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { faChevronLeft, faFilm, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 import 'bootstrap/dist/css/bootstrap.css';
 
-library.add(faChevronLeft, faChevronRight)
+library.add(faChevronLeft, faChevronRight, faFilm)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
